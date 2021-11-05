c8bf6ca9ab3ef88eac2655ac5fa1a5bd45c0bdcd
Luis Forero <luis.enrique.forero@hotmail.com>
2021-11-05 16:41:24 -0500

    ejercicio 5 y 6

---
+++ ejercicio5.js
@@ -0,0 +1,10 @@
+let x=300;
+let suma=0;
+while(x>0){
+    if(x%5==0){
+        console.log(x);
+        suma=suma + x
+    }
+    x=x-1
+}
+console.log("el resultado es " + suma)
---
+++ ejercicio6.js
@@ -0,0 +1,8 @@
+for(x=300;x>0;x--){
+    if(x%5==0){
+        console.log(x)
+        suma=suma + x
+    }
+    
+}
+console.log("el resultado es "+ suma)
